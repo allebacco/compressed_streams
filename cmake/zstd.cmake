05d30b42f93decbd7383219bda7d5f5973aa70db
Alessandro Bacchini <a.bacchini@camlintechnologies.com>
2019-01-07 09:46:19 +0100

    Project fixes

--- cmake/zstd.cmake
+++ cmake/zstd.cmake
@@ -4,12 +4,11 @@ include(ExternalProject)
 ExternalProject_Add(libzstd
     GIT_REPOSITORY    https://github.com/facebook/zstd.git
     GIT_TAG           master
-    SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/libzstd-src/build/cmake"
-    BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/libzstd-build"
+    SOURCE_DIR        ${CMAKE_CURRENT_BINARY_DIR}/libzstd-prefix/src/libzstd/build/cmake
     CMAKE_ARGS        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DZSTD_BUILD_PROGRAMS=OFF -DZSTD_BUILD_SHARED=OFF -DZSTD_LEGACY_SUPPORT=OFF
     INSTALL_COMMAND   ""
 )
 
-set(ZSTD_LIB_DIR ${CMAKE_CURRENT_BINARY_DIR}/libzstd-build/lib)
+set(ZSTD_LIB_DIR ${CMAKE_CURRENT_BINARY_DIR}/libzstd-prefix/src/libzstd-build/lib)
 set(ZSTD_LIBRARY ${ZSTD_LIB_DIR}/libzstd.a)
-set(ZSTD_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/libzstd-src/lib)
+set(ZSTD_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/libzstd-prefix/src/libzstd/lib)
